6f5dae3dd3fe4380b921ae77f40b56f369b07225
Ivan Illarionov <s25134@pjwstk.edu.pl>
2023-05-28 22:41:39 +0200

    first ver.

---
+++ BruteForce/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace BruteForce;
+
+internal class Program
+{
+    //private static int _maxCapacity;
+    private static int _maxWeight;
+    
+    private static string _bestCombination;
+    private static int _bestWeight;
+    private static int _bestWartosc;
+
+    private class Item
+    {
+        private int _wartosc;
+        private int _waga;
+
+        public int Wartosc
+        {
+            get => _wartosc;
+            set => _wartosc = value;
+        }
+
+        public int Waga
+        {
+            get => _waga;
+            set => _waga = value;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var path = @"C:\Users\r3mm1\Desktop\plecak.txt";
+        var lines = File.ReadLines(path).ToList();
+
+        _maxWeight = Convert.ToInt32(lines[0].Split(" ")[0]);
+        //_maxCapacity = Convert.ToInt32(lines[0].Split(" ")[1]);
+
+        var items = new List<Item>();
+        var config1 = lines[1].Split(",");
+        var config2 = lines[2].Split(",");
+
+        for (int i = 0; i < config1.Length; i++)
+        {
+            items.Add(new Item()
+            {
+                Wartosc = Convert.ToInt32(config1[i]),
+                Waga = Convert.ToInt32(config2[i]),
+            });
+        }
+
+        for (int i = 1; i <= Math.Pow(2,items.Count); i++)
+        {
+            var bin = Convert.ToString(i, 2);
+        
+            var extend = "";
+            if (bin.Length < items.Count)
+            {
+                for (int j = 0; j < items.Count - bin.Length; j++)
+                {
+                    extend += "0";
+                }
+                extend += bin;
+                bin = extend;
+            }
+            
+            
+            var tmpWartosc = 0;
+            var tmpWeight = 0;
+            var fit = true;
+            
+            for (int j = 0; j < bin.Length; j++)
+            {
+                if (bin[j] == '1')
+                {
+                    if (tmpWeight + items.ElementAt(j).Waga <= _maxWeight)
+                    {
+                        tmpWeight += items.ElementAt(j).Waga;
+                        tmpWartosc += items.ElementAt(j).Wartosc;
+                    }
+                    else
+                    {
+                        fit = false;
+                        break;
+                    }
+                }
+            }
+            if (fit && tmpWartosc > _bestWartosc)
+            {
+                _bestCombination = bin;
+                _bestWeight = tmpWeight;
+                _bestWartosc = tmpWartosc;
+                Console.Out.WriteLine("Successfully found a new combination: " + _bestCombination + " with weight = " + _bestWeight + " and value = " + _bestWartosc);
+            }
+        }
+
+        Console.Out.WriteLine("======================");
+        Console.Out.WriteLine("BruteForce completed.");
+        Console.Out.WriteLine("Founded the following values: ");
+        Console.Out.WriteLine("Best value: " + _bestWartosc);
+        Console.Out.WriteLine("Best weight: " + _bestWeight);
+        Console.Out.WriteLine("Best combination (binary): " + _bestCombination);
+
+
+
+
+        // var test = Convert.ToString(3, 2);
+        // Console.Out.WriteLine(test);
+        // var tt = "";
+        // for (int i = 0; i < items.Count - test.Length; i++)
+        // {
+        //     tt += "0";
+        // }
+        //
+        // tt += test;
+        // test = tt;
+        // Console.Out.WriteLine(test);
+        // Console.Out.WriteLine(test.Length);
+
+
+
+
+        // var test = "011001001";
+        // Console.Out.WriteLine(items[1].Wartosc);
+        //
+        // Console.Out.WriteLine(test[1]);
+        // var t = Int32.Parse(test[1].ToString());
+        // Console.Out.WriteLine(t);
+        //
+        // Console.Out.WriteLine(items[t].Wartosc);
+
+
+
+    }
+}
---
+++ K-Means/Program.cs
@@ -0,0 +1,186 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // crearing k from args and list from file
+        var k = Convert.ToInt32(args[0]);
+        List<double[]> vectors = ReadFile(args[1]);
+
+        // run k-means clustering with k=3 and maxIterations=100
+        KMeans kMeans = new KMeans(k, 10, vectors);
+        kMeans.Cluster();
+
+        // print clusters
+        List<List<double[]>> clusters = kMeans.GetClusters();
+        for (int i = 0; i < clusters.Count; i++) 
+        {
+            Console.Out.WriteLine("Cluster " + (i+1) + ":");
+            var cluster = clusters.ElementAt(i);
+            foreach (var vector in cluster) 
+            {
+                Console.Out.WriteLine("(" + vector[0] + ", " + vector[1] + ", " + vector[2] + ")");
+            }
+
+            Console.Out.WriteLine("");
+        }
+    }
+
+    public static List<double[]> ReadFile(string path)
+    {
+        var vectors = new List<double[]>();
+        var lines = File.ReadLines(path);
+        foreach (string line in lines)
+        {
+            var settings = line.Split(",");
+            var doubles = new double[settings.Length];
+            for (int i = 0; i < settings.Length; i++)
+            {
+                settings[i] = settings[i].Replace(".", ",");
+                doubles[i] = Convert.ToDouble(settings[i]);
+            }
+            vectors.Add(doubles);
+        }
+        return vectors;
+    }
+
+
+
+    
+
+    private class KMeans
+    {
+        private int _k; // number of clusters
+        private int _maxIterations; // maximum number of iterations
+        private List<double[]> _vectors; // input vectors
+        private List<double[]> _centroids; // centroids of clusters
+
+
+        public KMeans(int k, int maxIterations, List<double[]> vectors)
+        {
+            _k = k;
+            _maxIterations = maxIterations;
+            _vectors = vectors;
+
+            var alreadyTakenIndexes = new List<int>();
+            var random = new Random();
+            int x = random.Next(0, 10) * vectors.Count;
+            
+            _centroids.Add(vectors.ElementAt(x));
+            alreadyTakenIndexes.Add(x);
+
+            for (int i = 1; i < k; i++) 
+            {
+                while (alreadyTakenIndexes.Contains(x))
+                {
+                    x = random.Next() * vectors.Count;
+                }
+
+                _centroids.Add(vectors.ElementAt(x));
+                alreadyTakenIndexes.Add(x);
+            }
+            Console.Out.WriteLine("Centroid list size is: " + _centroids.Count);
+        }
+        
+        
+        public void Cluster() {
+            List<Double> euclideanSums;
+            // repeat until convergence or maximum iterations reached
+            for (int iteration = 0; iteration < _maxIterations; iteration++) 
+            {
+                // checking if previous sum is not the same
+
+                euclideanSums = new List<double>();
+                // initialize clusters and k 0's for sum at iteration start
+                var clusters = new List<List<double[]>>();
+                for (int i = 0; i < _k; i++) 
+                {
+                    clusters.Add(new List<double[]>());
+                    euclideanSums.Add(0.0);
+                }
+
+                // assign vectors to nearest centroid
+
+                foreach (var vector in _vectors)
+                {
+                    var nearestCentroid = GetNearestCentroid(vector);
+                    var tmp = euclideanSums.ElementAt(nearestCentroid);
+                    tmp += EuclideanDistance(vector, _centroids.ElementAt(nearestCentroid));
+                    euclideanSums[nearestCentroid] = tmp;
+                    clusters.ElementAt(nearestCentroid).Add(vector);
+                }
+                
+                
+
+                // update centroids
+                for (int i = 0; i < _k; i++) 
+                {
+                    var newCentroid = new double[3];
+                    var cluster = clusters.ElementAt(i);
+                    if (cluster.Count != 0) 
+                    {
+                        for (int j = 0; j < 3; j++) 
+                        {
+                            var sum = 0.0;
+                            foreach (var vector in cluster) 
+                            {
+                                sum += vector[j];
+                            }
+                            newCentroid[j] = sum / cluster.Count;
+                        }
+                    } 
+                    else
+                    {
+                        newCentroid = _centroids.ElementAt(i);
+                    }
+                    _centroids[i] = newCentroid;
+                }
+                Console.Out.WriteLine("Iteration number: " + iteration + " with sums: " + euclideanSums);
+            }
+        }
+        
+        public int GetNearestCentroid(double[] vector) 
+        {
+            int nearestCentroid = 0;
+            double nearestDistance = Double.MaxValue;
+            for (int i = 0; i < _k; i++) 
+            {
+                double[] centroid = _centroids.ElementAt(i);
+                double distance = EuclideanDistance(vector, centroid);
+                if (distance < nearestDistance) 
+                {
+                    nearestCentroid = i;
+                    nearestDistance = distance;
+                }
+            }
+            return nearestCentroid;
+        }
+        
+        public double EuclideanDistance(double[] a, double[] b) 
+        {
+            double sum = 0.0;
+            for (int i = 0; i < 3; i++) 
+            {
+                double diff = a[i] - b[i];
+                sum += diff * diff;
+            }
+            return Math.Sqrt(sum);
+        }
+        
+        public List<List<double[]>> GetClusters() 
+        {
+            var clusters = new List<List<double[]>>();
+            for (int i = 0; i < _k; i++) 
+            {
+                clusters.Add(new List<double[]>());
+            }
+            foreach (var vector in _vectors) 
+            {
+                int nearestCentroid = GetNearestCentroid(vector);
+                clusters.ElementAt(nearestCentroid).Add(vector);
+            }
+            return clusters;
+        }
+        
+        
+    }
+}
