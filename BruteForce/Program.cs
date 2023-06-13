159e83b6e95ab0505ba4526705fdb9659ac3ce89
Ivan Illarionov <s25134@pjwstk.edu.pl>
2023-06-13 23:49:37 +0200

    ver 2

--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -5,8 +5,8 @@ internal class Program
     private static int _maxWeight;
     
     private static string _bestCombination = null!;
-    private static int _bestWeight;
-    private static int _bestWartosc;
+    private static int _bestWeight = 0;
+    private static int _bestWartosc = 0;
 
     private class Item
     {
@@ -28,11 +28,48 @@ internal class Program
     
     static void Main(string[] args)
     {
-        var path = @"C:\Users\r3mm1\Desktop\plecak.txt";
+        var items = Config(@"C:\Users\r3mm1\Desktop\plecak.txt");
+        
+        for (int i = 1; i <= Math.Pow(2, items.Count); i++)
+        {
+            var bin = Convert_binary(i, items.Count);
+            
+            var tmpWartosc = 0;
+            var tmpWeight = 0;
+
+            for (int j = 0; j < bin.Length; j++)
+            {
+                if (bin[j] == '1')
+                {
+                    tmpWeight += items.ElementAt(j).Waga;
+                    tmpWartosc += items.ElementAt(j).Wartosc;
+                }
+            }
+            if (tmpWeight <= _maxWeight)
+            {
+                if (tmpWartosc > _bestWartosc)
+                {
+                    _bestCombination = bin;
+                    _bestWeight = tmpWeight;
+                    _bestWartosc = tmpWartosc;
+                    Console.Out.WriteLine("Successfully found a new combination: " + _bestCombination + " with weight = " + _bestWeight + " and value = " + _bestWartosc);
+                }
+            }
+        }
+        
+        Console.Out.WriteLine("======================");
+        Console.Out.WriteLine("BruteForce completed.");
+        Console.Out.WriteLine("Founded the following values: ");
+        Console.Out.WriteLine("Best value: " + _bestWartosc);
+        Console.Out.WriteLine("Best weight: " + _bestWeight);
+        Console.Out.WriteLine("Best combination (binary): " + _bestCombination);
+    }
+    
+    
+    private static List<Item> Config(string path)
+    {
         var lines = File.ReadLines(path).ToList();
-
         _maxWeight = Convert.ToInt32(lines[0].Split(" ")[0]);
-
         var items = new List<Item>();
         var config1 = lines[1].Split(",");
         var config2 = lines[2].Split(",");
@@ -45,69 +82,49 @@ internal class Program
                 Waga = Convert.ToInt32(config2[i]),
             });
         }
-
-        for (int i = 1; i <= Math.Pow(2,items.Count); i++)
-        {
-            var bin = Convert.ToString(i, 2);
-            
-            if (bin.Length < items.Count)
-            {
-                var extend = "";
-                for (int j = 0; j < items.Count - bin.Length; j++)
-                {
-                    extend += "0";
-                }
-                extend += bin;
-                bin = extend;
-            }
-            
-            var tmpWartosc = 0;
-            var tmpWeight = 0;
-            var fit = true;
-            
-            for (int j = 0; j < bin.Length; j++)
-            {
-                if (bin[j] == '1')
-                {
-                    if (tmpWeight + items.ElementAt(j).Waga <= _maxWeight)
-                    {
-                        tmpWeight += items.ElementAt(j).Waga;
-                        tmpWartosc += items.ElementAt(j).Wartosc;
-                    }
-                    else
-                    {
-                        fit = false;
-                        break;
-                    }
-                }
-            }
-            if (fit && tmpWartosc > _bestWartosc)
-            {
-                _bestCombination = bin;
-                _bestWeight = tmpWeight;
-                _bestWartosc = tmpWartosc;
-                Console.Out.WriteLine("Successfully found a new combination: " + _bestCombination + " with weight = " + _bestWeight + " and value = " + _bestWartosc);
-            }
-        }
-        
-        Console.Out.WriteLine("======================");
-        Console.Out.WriteLine("BruteForce completed.");
-        Console.Out.WriteLine("Founded the following values: ");
-        Console.Out.WriteLine("Best value: " + _bestWartosc);
-        Console.Out.WriteLine("Best weight: " + _bestWeight);
-        Console.Out.WriteLine("Best combination (binary): " + _bestCombination);
-        
-        
-        //Версия на StringBuilder, но эта версия жрет еще больше памяти...
-        // var extend = new StringBuilder();
-        // if (bin.Length < items.Count)
-        // {
-        //     for (int j = 0; j < items.Count - bin.Length; j++)
-        //     {
-        //         extend.Append("0");
-        //     }
-        //     extend.Append(bin);
-        //     bin = extend.ToString();
-        // }
+        return items;
     }
+    
+
+    private static string Convert_binary(int i, int lenght)
+    {
+        var bin = Convert.ToString(i, 2);
+            
+        if (bin.Length < lenght)
+        {
+            var extend = "";
+            for (int j = 0; j < lenght - bin.Length; j++)
+            {
+                extend += "0";
+            }
+            extend += bin;
+            bin = extend;
+        }
+
+        return bin;
+    }
+
+    //Версия 2 на StringBuilder, но эта версия жрет еще больше памяти...
+    // var extend = new StringBuilder();
+    // if (bin.Length < items.Count)
+    // {
+    //     for (int j = 0; j < items.Count - bin.Length; j++)
+    //     {
+    //         extend.Append("0");
+    //     }
+    //     extend.Append(bin);
+    //     bin = extend.ToString();
+    // }
+        
+    //Версия 1
+    // if (bin.Length < items.Count)
+    // {
+    //     var extend = "";
+    //     for (int j = 0; j < items.Count - bin.Length; j++)
+    //     {
+    //         extend += "0";
+    //     }
+    //     extend += bin;
+    //     bin = extend;
+    // }
 }
